093e95acb2e910cf52fa61d38846d4f15b1da1c8
Daryaskhrv <daryasheqer@yandex.ru>
2024-05-13 17:25:18 +0400

    change BoldTextDirective

--- digital-depaartment-application/src/app/src/app/directives/bold-text.directive.ts
+++ digital-depaartment-application/src/app/src/app/directives/bold-text.directive.ts
@@ -1,11 +1,14 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Renderer2, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[appBoldText]',
   standalone: true
 })
-export class BoldTextDirective {
+export class BoldTextDirective implements OnInit {
 
-  constructor() { }
+  constructor(private el: ElementRef, private renderer: Renderer2) { }
 
+  ngOnInit() {
+    this.renderer.setStyle(this.el.nativeElement, 'font-weight', 'bold');
+  }
 }
